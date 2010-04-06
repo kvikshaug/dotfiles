e870fe4117035db941b299e6d224ef8aea21ba77
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-04-06 17:56:16 +0200

    'l' -> 'll'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set PATH $PATH /home/murray/apps/git-achievements /home/murray/apps/showoff-krisb/bin
 
+function l -d "ll shortcut"; ll $argv; end
 function mv -d "Be verbose"; mv -v $argv; end
 function cp -d "Be verbose"; cp -v $argv; end
 function rm -d "Be verbose, and confirm removal"; rm -iv $argv; end
