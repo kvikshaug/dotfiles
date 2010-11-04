9af4dd881194249acc89aebc9197fd372c2a6ea5
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-11-04 16:48:17 +0100

    Use scala to parse atom feed XML

---
+++ .dwmsb-mail-filter.scala
@@ -0,0 +1,17 @@
+/**
+ * Parses atom feed XML for gmail into a line showing:
+ * XM Y: Z
+ * Where:
+ * X = The number of unread mails in the inbox
+ * Y = The name of the most recent sender in the conversation
+ * Z = The subject of the e-mail
+ **/
+import scala.xml.XML
+
+val xml = XML.load(args(0))
+val mailcount = (xml \ "entry").size
+if(mailcount > 0) {
+  val author = ((xml \ "entry")(0) \ "author" \ "name").text
+  val subject = ((xml \ "entry")(0) \ "title").text
+  print(mailcount + "M " + author + ": " + subject + " | ")
+}
