71c77062cde8636735b6b1e7fc93952cf6edecb5
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-12-03 21:29:06 +0100

    Add bg script

---
+++ .bg/setbg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding: utf-8
+import os, time, subprocess
+
+sleeptime = 5 # seconds
+
+# Hardcode
+subprocess.call(["xsetbg", "/home/murray/.bg/img/pink.jpg")
+
+# Iterate
+#i = 0
+#while True:
+#  files = os.listdir("/home/murray/.bg/img/")
+#  if(i >= len(files)):
+#    i = 0
+#  subprocess.call(["xsetbg", "/home/murray/.bg/img/" + files[i]])
+#  i += 1
+#  time.sleep(sleeptime)
+
